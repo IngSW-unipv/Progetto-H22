251c4e8bef872a6902d3faedc93cdfcc30b386d7
hamza-abbad01 <hamza.abbad01@universitadipavia.it>
2022-02-26 11:33:11 +0100

    commit unversioned files

---
+++ src/it.unipv.po.nsm.aerospin/java/model/persistence/PDFTest.java
@@ -0,0 +1,36 @@
+package model.persistence;
+
+
+import java.io.IOException;
+
+public class PDFTest {
+
+
+    public static void main(String[] args) throws IOException {
+        /*File file = new File("src/it.unipv.po.nsm.aerospin/resources/GeneratedPDF/template.pdf");
+        PDDocument document =  PDDocument.load(file);
+        PDPage page = document.getPage(0);
+
+        PDPageContentStream contentStream = new PDPageContentStream(document, page, PDPageContentStream.AppendMode.APPEND, true, true);
+        contentStream.beginText();
+        contentStream.setFont(PDType1Font.COURIER, 12);
+        contentStream.newLineAtOffset(115, 688);
+        String text = "Pablo";
+        contentStream.showText(text);
+        contentStream.newLineAtOffset(0,-24);
+        contentStream.showText("Lamezia");
+        contentStream.newLineAtOffset(0,-24);
+        contentStream.showText("Dubai Int.");
+        contentStream.endText();
+        contentStream.close();
+        document.save("src/it.unipv.po.nsm.aerospin/resources/GeneratedPDF/boardingPass.pdf");
+        document.close();*/
+
+
+    }
+}
+
+
+
+
+
