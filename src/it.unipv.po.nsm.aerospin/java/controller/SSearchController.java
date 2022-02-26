d7a209f9c6581bb576fb6c8c457f10889e5fc81e
Dm0r18 <dvdmorano@gmail.com>
2022-02-26 23:18:27 +0100

    Fining Search Screen with Controller

--- src/it.unipv.po.nsm.aerospin/java/controller/SSearchController.java
+++ src/it.unipv.po.nsm.aerospin/java/controller/SSearchController.java
@@ -1,12 +1,14 @@
 package controller;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.skin.ComboBoxListViewSkin;
+import javafx.scene.input.KeyCode;
 import javafx.util.Callback;
-import model.persistence.service.AirportService;
 import view.MainApplication;
 import view.ScreensController;
 
@@ -31,16 +33,29 @@ public class SSearchController implements Initializable, IControlledScreen {
     private DatePicker date2;
 
     @FXML
-    private RadioButton ar;
-
-    @FXML
     private RadioButton oneway;
 
     @FXML
-    private ToggleGroup group = new ToggleGroup();
-
-    @FXML
     private Button cerca = new Button();
+
+    private final ObservableList<String> options = FXCollections.observableArrayList(
+            "Aab",
+            "Aer",
+            "Aeq",
+            "Arx",
+            "Byad",
+            "Csca",
+            "Csee",
+            "Cfefe",
+            "Cead",
+            "Defea",
+            "Dqeqe",
+            "Fefaf",
+            "Gert",
+            "Wqad",
+            "Xsad",
+            "Zzz"
+    );
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -49,20 +64,14 @@ public class SSearchController implements Initializable, IControlledScreen {
 //        strings = AirportService.findAll();
         strings.add("Test1");
         strings.add("Test2");
-        cb1.setItems((FXCollections.observableArrayList(strings)));
-        cb2.setItems((FXCollections.observableArrayList(strings)));
+        cb1.setItems(options);
+        cb2.setItems(options);
         //gestire cb1 == cb2!!!!
 
-//       Seleziona dalla combobox in base alla lettera selezionata??
-//        cb1.textProperty().addListener((observable, oldValue, newValue) -> {
-//            for (int i = 0; i < comboBoxItems.size(); i++) {
-//                if (comboBoxItems.get(i).toLowerCase().contains(textField.getText().toLowerCase())) {
-//                    myComboBox.getSelectionModel().select(i);
-//                    myComboBox.show();
-//                }
-//            }
-//        });
-        
+        selectOptionOnKey(cb1, options);
+        selectOptionOnKey(cb2, options);
+
+        // Metodo per mantenere le date selezionabili tra Oggi e prossimi 4 Mesi
         Callback<DatePicker, DateCell> callDate = new Callback<DatePicker, DateCell>() {
             @Override
             public DateCell call(final DatePicker param) {
@@ -81,9 +90,8 @@ public class SSearchController implements Initializable, IControlledScreen {
         date2.setDayCellFactory(callDate);
         //gestire date2 < date1!!!!
 
-        ar.setToggleGroup(group);
-        ar.setSelected(true);
-        oneway.setToggleGroup(group);
+
+        // Se seleziono Solo Andata non posso inserire Data Ritorno
         date2.disableProperty().bind(oneway.selectedProperty());
 
 //        BooleanBinding booleanBind =    cb1.valueProperty().isNull()
@@ -98,6 +106,41 @@ public class SSearchController implements Initializable, IControlledScreen {
 
     public void setScreenParent(ScreensController screenParent) {
         myController = screenParent;
+    }
+
+    /* When you press a letter key
+     *  this method will search for an option(item) that starts with the input letter key
+     *  and it selects the first occurrence in combo box
+     */
+    public void selectOptionOnKey(ComboBox<String> cb, List<String> strings) {
+        cb.setOnKeyPressed(e -> {
+            KeyCode keyCode = e.getCode();
+
+            if (keyCode.isLetterKey()) {
+                char key = keyCode.getName().charAt(0);
+
+                SingleSelectionModel<String> cbSelectionModel = cb.getSelectionModel();
+
+                cbSelectionModel.select(0);
+
+                for (int i = 0; i < strings.size(); i++) {
+                    if(cbSelectionModel.getSelectedItem().charAt(0) == key) {
+                        // option which starts with the input letter found -> select it
+//                        cbSelectionModel.select(i);
+                        cbSelectionModel.select(i);
+                        ComboBoxListViewSkin<?> skin = (ComboBoxListViewSkin<?>) cb.getSkin();
+                        ListView<?> list = (ListView<?>) skin.getPopupContent();
+                        list.scrollTo(i);
+                        /* Before exiting the function it would be nice if after the selection,
+                           the combo box would auto slide/jump to the option which is selected.
+                           I don't know how to do that. */
+                        return;
+                    }
+                    else
+                        cbSelectionModel.selectNext();
+                }
+            }
+        });
     }
 
     @FXML
