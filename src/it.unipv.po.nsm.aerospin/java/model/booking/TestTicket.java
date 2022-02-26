8ce2d4a5a09182e96142150ade693476f9e86f4f
Davide Morano <66534866+Dm0r18@users.noreply.github.com>
2022-02-27 00:35:19 +0100

    Update TestTicket.java

--- src/it.unipv.po.nsm.aerospin/java/model/booking/TestTicket.java
+++ src/it.unipv.po.nsm.aerospin/java/model/booking/TestTicket.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class TestTicket {
     public static void main(String[] args) throws IOException, WriterException {
-        Ticket ticket = new Ticket("Antonino","Nocera","SUF","MXP","AES736","28/02/2022","8:00");
+        Ticket ticket = new Ticket("Pablo","Escobar","SUF","MXP","AES736","28/02/2022","8:00");
         ticket.generateTicket();
     }
 }
