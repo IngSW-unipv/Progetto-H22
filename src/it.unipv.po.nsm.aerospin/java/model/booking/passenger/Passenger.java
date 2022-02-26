71431c0c6dd3e04596879f4c08ef8722231824d3
matteo <matteo.invernizzi01@universitadipavia.it>
2022-02-26 11:26:24 +0100

    Modifica classe Booking, passenger e eliminazione di luggage

--- src/it.unipv.po.nsm.aerospin/java/model/booking/passenger/Passenger.java
+++ src/it.unipv.po.nsm.aerospin/java/model/booking/passenger/Passenger.java
@@ -1,7 +1,44 @@
 package model.booking.passenger;
 
-public class Passenger {
 
+
+
+public class Passenger{
+
+    int id;
+    ClassType classType;
     AgeGroup ageGroup;
+    short eta;
 
+    public int getId() {
+        return id;
+    }
+    public ClassType getClassType() {
+        return classType;
+    }
+
+    public void setClassType(ClassType classType) {
+        this.classType = classType;
+    }
+
+    public AgeGroup getAgeGroup() {
+        return ageGroup;
+    }
+
+    private void setAgeGroup(int eta) {
+        if (eta <= AgeGroup.INFANTS.getMaxAge())
+            ageGroup = AgeGroup.INFANTS;
+        else if (eta <= AgeGroup.CHILDREN.getMaxAge())
+            ageGroup = AgeGroup.CHILDREN;
+        else if (eta <= AgeGroup.ADULTS.getMaxAge())
+            ageGroup = AgeGroup.CHILDREN;
+
+    }
+
+    public Passenger(int id, String nome, String cognome, short eta) {
+        super(nome, cognome);
+        this.eta = eta;
+        this.id = id;
+        setAgeGroup(eta);
+    }
 }
