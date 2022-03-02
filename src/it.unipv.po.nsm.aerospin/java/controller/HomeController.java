8d5103830ea171d87213f27667b09b8f5450d675
Dm0r18 <dvdmorano@gmail.com>
2022-03-02 12:35:46 +0100

    Different Screen Structure

--- src/it.unipv.po.nsm.aerospin/java/controller/HomeController.java
+++ src/it.unipv.po.nsm.aerospin/java/controller/HomeController.java
@@ -1,53 +1,15 @@
 package controller;
 
-import com.jfoenix.controls.JFXButton;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import util.Session;
-import view.Factory;
-import view.ScreensController;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class HomeController implements Initializable, IControlledScreen {
+public class HomeController implements Initializable {
 
-    private ScreensController myController;
-    private Factory factory = Factory.getInstance();
-
-    @FXML
-    JFXButton jBtn = new JFXButton();
 
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-
-        jBtn.visibleProperty().bind(new SimpleBooleanProperty(factory.getSession().isLogged()));
+    public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
-
-    @Override
-    public void setScreenParent(ScreensController screenParent) {
-        myController = screenParent;
-    }
-
-    @FXML
-    private void goToSearch(ActionEvent event){
-        myController.setScreen(factory.getSearch());
-    }
-
-    @FXML
-    private void goToLogin(ActionEvent event){
-        myController.setScreen(factory.getLogin());
-    }
-
-    @FXML
-    private void logout(ActionEvent event){
-        //cambia stato come non loggato
-        factory.getSession().setLogged(false);
-        myController.setScreen(factory.getHome());
-        jBtn.visibleProperty().bind(new SimpleBooleanProperty(factory.getSession().isLogged()));
-    }
-
 }
