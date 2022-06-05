4357c1bb3c093bd1dae7305bb94fbaf751d7dc6f
Hamza <hamza.abbad01@universitadipavia.it>
2022-06-05 09:01:12 +0200

    working on Orders

--- src/it.unipv.po.nsm.aerospin/java/model/persistence/service/OrdersService.java
+++ src/it.unipv.po.nsm.aerospin/java/model/persistence/service/OrdersService.java
@@ -21,17 +21,17 @@ public class OrdersService {
         return orders;
     }
 
-    public Orders findByUserId(int userId) {
+    public List<Orders> findByUserId(int userId) {
         ordersDao.getConn().openCurrentSession();
         List<Orders> orders = ordersDao.findAll();
         ordersDao.getConn().closeCurrentSession();
-        return orders.stream().filter(o -> o.getPassengerByPassengerId().getUserByUserId().getId() == userId).collect(java.util.stream.Collectors.toList()).get(0);
+        return orders.stream().filter(o -> o.getPassengerByPassengerId().getUserByUserId().getId() == userId).collect(java.util.stream.Collectors.toList());
     }
-    public Orders findByEmail(String email) {
+    public List<Orders> findByEmail(String email) {
         ordersDao.getConn().openCurrentSession();
         List<Orders> orders = ordersDao.findAll();
         ordersDao.getConn().closeCurrentSession();
-        return orders.stream().filter(o -> o.getPassengerByPassengerId().getUserByUserId().getEmail() == email).collect(java.util.stream.Collectors.toList()).get(0);
+        return orders.stream().filter(o -> o.getPassengerByPassengerId().getUserByUserId().getEmail() == email).collect(java.util.stream.Collectors.toList());
     }
 
 
